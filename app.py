c8df2fd275be9833e2cecbccc7882a7ad519a3b5
romvano <romvano@gmail.com>
2017-09-11 16:33:40 +0300

    added user blueprint to app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_login import LoginManager
 
 from api.host import host_bp
 from api.client import client_bp
+from api.user import user_bp
 from extentions import setup, mongo, lm, JSONEncoder
 import config
 
@@ -15,6 +16,7 @@ app.secret_key = 'Afe454_gjklr993mkl2nFsdfGRrrggReQcBmm'
 app.register_blueprint(host_bp, url_prefix='/api/host/')
 app.register_blueprint(host_bp, url_prefix='/api/barmen/')
 app.register_blueprint(client_bp, url_prefix='/api/client/')
+app.register_blueprint(user_bp, url_prefix='api/user/')
 
 lm.init_app(app)
 
