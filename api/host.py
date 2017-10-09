941638e46084923e32de5fb4a5d2f9d3e35c21a3
romvano <romvano@gmail.com>
2017-10-10 02:13:43 +0300

    upload image host_id from session

--- api/host.py
+++ api/host.py
@@ -237,14 +237,12 @@ def get_file(filename):
 @host_bp.route('upload/', methods=['POST'])
 @login_required
 def upload():
-    uid = get_request_data(request).get('host_id')
+    uid = session.get('host_id')
     if not uid:
-        return jsonify({'message': "Host id must be provided"}), HTTP_400_BAD_REQUEST
+        return jsonify({'message': "Host id must be provided"}), HTTP_403_FORBIDDEN
     host = Host(uid=uid)
     if host.uid is None:
         return jsonify({'message': "No host with uid="+uid+" in db"}), HTTP_404_NOT_FOUND
-    if current_user.uid != host.owner_uid:
-        return jsonify({'message': "You are not this host"}), HTTP_403_FORBIDDEN
     # name the new file based on host uid
     current_picture_name = host.logo
     if current_picture_name is None:
